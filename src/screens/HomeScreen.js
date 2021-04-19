82d7fdab7328c9e804b9794b0a5ad70fe7404c8b
themaleem <zizo9585@gmail.com>
2021-04-19 17:38:43 +0100

    news section done

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import { getNews } from "../actions/api";
 
 import Hero from "../components/hero";
+import NewsSection from "../components/NewsSection";
 
 const HomeScreen = () => {
+  const [fetchedNews, setFetchedNews] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchNews = async () => {
+    await setLoading(true);
+    const { data } = await getNews();
+    setFetchedNews(data.articles);
+    setTimeout(() => {
+      setLoading(true);
+    }, 1000);
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchNews();
+
+    setLoading(true);
+  }, []);
+
   return (
     <>
       <Hero />
+      <NewsSection loading={loading} fetchedNews={fetchedNews} />
     </>
   );
 };
